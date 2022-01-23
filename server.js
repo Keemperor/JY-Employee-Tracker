efb075c92d64dc0d6abc808db750d0b3e9c2dad6
Keemperor <jacobotau@hotmail.com>
2022-01-23 16:23:59 -0500

    created connection functions

--- server.js
+++ server.js
@@ -8,5 +8,10 @@ const connection = mysql.createConnection({
   user: 'root',
   password:  '...',
   database: 'employee_db'
+});
 
+connection.connect(function(err) {
+  if (err) throw err
+  console.log("Connected as Id" + connection.threadId)
+  startPrompt();
 });
