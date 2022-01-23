2b8ffbf8af806f988194639b3929b4946b8a4a94
Keemperor <jacobotau@hotmail.com>
2022-01-23 16:22:12 -0500

    initiated server arguments

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const cTable = require('console.table');
 
 const connection = mysql.createConnection({
   host: 'localhost',
+  port: 3306,
   user: 'root',
-  database: 'test'
+  password:  '...',
+  database: 'employee_db'
+
 });
