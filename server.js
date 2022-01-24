ef86545c337bcb998f873660ad0e753d8c686340
Keemperor <jacobotau@hotmail.com>
2022-01-23 21:11:41 -0500

    added add department function

--- server.js
+++ server.js
@@ -17,53 +17,52 @@ connection.connect(function(err) {
 });
 
 function options() {
-  inquirer
-      .prompt({
-          name: 'action',
-          type: 'list',
-          message: 'Welcome to our employee database! Please choose an option',
-          choices: [
-                  'View all employees',
-                  'View all departments',
-                  'View all roles',
-                  'Add an employee',
-                  'Add a department',
-                  'Add a role',
-                  'Update employee role',
-                  'Delete an employee',
-                  'EXIT'
-                  ]
-          }).then(function (answer) {
-              switch (answer.action) {
-                  case 'View all employees':
-                      viewEmployees();
-                      break;
-                  case 'View all departments':
-                      viewDepartments();
-                      break;
-                  case 'View all roles':
-                      viewRoles();
-                      break;
-                  case 'Add an employee':
-                      addEmployee();
-                      break;
-                  case 'Add a department':
-                      addDepartment();
-                      break;
-                  case 'Add a role':
-                      addRole();
-                      break;
-                  case 'Update employee role':
-                      updateRole();
-                      break;
-                  case 'Delete an employee':
-                      deleteEmployee();
-                      break;
-                  case 'EXIT': 
-                      exitApp();
-                      break;
-                  default:
-                      break;
+  inquirer.prompt({
+    name: 'action',
+    type: 'list',
+    message: 'Welcome to our employee database! Please choose an option',
+    choices: [
+      'View all employees',
+      'View all departments',
+      'View all roles',
+      'Add an employee',
+      'Add a department',
+      'Add a role',
+      'Update employee role',
+      'Delete an employee',
+      'EXIT'
+             ]
+       }).then(function (answer) {
+        switch (answer.action) {
+          case 'View all employees':
+            viewEmployees();
+            break;
+          case 'View all departments':
+            viewDepartments();
+            break;
+          case 'View all roles':
+            viewRoles();
+            break;
+          case 'Add an employee':
+            addEmployee();
+            break;
+          case 'Add a department':
+            addDepartment();
+            break;
+          case 'Add a role':
+            addRole();
+            break;
+          case 'Update employee role':
+            updateRole();
+            break;
+          case 'Delete an employee':
+            deleteEmployee();
+            break;
+          case 'EXIT': 
+            exitApp();
+            break;
+          default:
+            break;
               }
       })
 };
@@ -94,4 +93,83 @@ function viewRoles() {
       console.table('All Roles:', res);
       options();
   })
+};
+
+function addEmployee() {
+  connection.query('SELECT * FROM role', function (err, res) {
+    if (err) throw err;
+    inquirer.prompt([
+              {
+                  name: 'first_name',
+                  type: 'input', 
+                  message: "What is the employee's fist name? ",
+              },
+              {
+                  name: 'last_name',
+                  type: 'input', 
+                  message: "What is the employee's last name? "
+              },
+              {
+                  name: 'manager_id',
+                  type: 'input', 
+                  message: "What is the employee's manager's ID? "
+              },
+              {
+                  name: 'role', 
+                  type: 'list',
+                  choices: function() {
+                  var roleArray = [];
+                  for (let i = 0; i < res.length; i++) {
+                      roleArray.push(res[i].title);
+                  }
+                  return roleArray;
+                  },
+                  message: "What is this employee's role? "
+              }
+              ]).then(function (answer) {
+                  let role_id;
+                  for (let a = 0; a < res.length; a++) {
+                      if (res[a].title == answer.role) {
+                          role_id = res[a].id;
+                          console.log(role_id)
+                      }                  
+                  }  
+                  connection.query(
+                  'INSERT INTO employee SET ?',
+                  {
+                      first_name: answer.first_name,
+                      last_name: answer.last_name,
+                      manager_id: answer.manager_id,
+                      role_id: role_id,
+                  },
+                  function (err) {
+                      if (err) throw err;
+                      console.log('Your employee has been added!');
+                      options();
+                  })
+              })
+      })
+};
+
+function addDepartment() {
+  inquirer.prompt([
+          {
+              name: 'newDepartment', 
+              type: 'input', 
+              message: 'Which department would you like to add?'
+          }
+          ]).then(function (answer) {
+              connection.query(
+                  'INSERT INTO department SET ?',
+                  {
+                      name: answer.newDepartment
+                  });
+              var query = 'SELECT * FROM department';
+              connection.query(query, function(err, res) {
+              if(err)throw err;
+              console.log('Your department has been added!');
+              console.table('All Departments:', res);
+              options();
+              })
+          })
 };
